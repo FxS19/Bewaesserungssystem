b4f11161345229fbb3ccb5af147d47b0829a3377
Felix Schmidt <felix.schmidt.1@gmx.de>
2022-07-24 23:06:21 +0200

    Hauptdatei hinzugefügt

---
+++ Bewaesserungssystem.ino
@@ -0,0 +1,27 @@
+/*
+ * Benötigt eine Datei 00_SSID.ino
+ * Mit dem Inhalt:
+ *
+ * #ifndef STASSID
+ * #define STASSID "<<SSID>>"
+ * #define STAPSK  "<<PASSWORD>>"
+ * #endif
+ */
+
+#include <string>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <RTC.h>
+#include <BH1750.h>
+#include <TZ.h>
+#include <Schedule.h> //?
+#include <coredecls.h> // settimeofday_cb()
+#include <time.h>
+#include <ESP8266WiFi.h>
+#include <sys/time.h>
+#include <FS.h>
+//#include <SPIFFSLogger.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <EEPROM.h>
+#include <Eeprom24C32_64.h>
